c8694755cc882c3fff1b2e2433e8528150e1df24
Pol <polmonteroalegre@gmail.com>
2024-04-07 16:05:49 +0200

    FEAT: Profilejsx and profileCss created and tested

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,157 +1,125 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router";
-import "./Profile.css";
+import { useNavigate } from "react-router-dom";
+import "./Profile.css"
 import { GetProfile, UpdateProfile } from "../../services/apiCalls";
 import { CInput } from "../../common/CInput/CInput";
-import dayjs from "dayjs";
+import { validame } from "../../utils/function";
 import { Header } from "../../common/Header/Header";
 import { CButton } from "../../common/CButton/CButton";
 
-export const Profile = () => {
-  const datosUser = JSON.parse(localStorage.getItem("passport"));
-  const navigate = useNavigate();
 
-  const [write, setWrite] = useState("disabled");
-  const [tokenStorage, setTokenStorage] = useState(datosUser?.token);
-  const [loadedData, setLoadedData] = useState(false);
+
+export const Profile = () => {
+  const datosUser = JSON.parse(localStorage.getItem("passport"))
+  const navigate = useNavigate()
+  const [write, setWrite] = useState("disabled")
+  const [tokenStorage, setTokenStorage] = useState(datosUser?.token)
+  const [loadedData, setLoadedData] = useState(false)
   const [user, setUser] = useState({
     name: "",
-    surname: "",
-    birth: "",
     email: "",
-  });
+  })
+
 
   const [userError, setUserError] = useState({
     nameError: "",
-    surnameError: "",
-    birthError: "",
     emailError: "",
-  });
+  })
+
+  const [msgError, setMsgError] = useState("")
 
   const inputHandler = (e) => {
     setUser((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-    }));
-  };
+    }))
+  }
 
   const checkError = (e) => {
-    //a gusto del consumidor....
-  };
+    const error = validame(e.target.name, e.target.value)
 
+    setUserError((prevState) => ({
+      ...prevState,
+      [e.target.name + "Error"]: error,
+    }))
+  }
   useEffect(() => {
     if (!tokenStorage) {
-      navigate("/");
+      navigate("/")
     }
-  }, [tokenStorage]);
+  }, [tokenStorage])
 
   useEffect(() => {
     const getUserProfile = async () => {
       try {
         const fetched = await GetProfile(tokenStorage);
-
         setLoadedData(true);
-
-        const parsedBirth = dayjs(fetched.data.birth).format("YYYY-MM-DD");
-
         setUser({
           name: fetched.data.name,
-          surname: fetched.data.surname,
-          birth: parsedBirth,
+          lastName: fetched.data.lastName,
           email: fetched.data.email,
         });
-
       } catch (error) {
-        console.log(error);
+        setMsgError(error.message);
       }
     };
-
     if (!loadedData) {
       getUserProfile();
     }
   }, [user]);
 
-  const updateData = async () => {
-
+  const updatedData = async () => {
     try {
-      const fetched = await UpdateProfile(tokenStorage, user) 
-
-      setUser({
-        name: fetched.data.name,
-        surname: fetched.data.surname,
-        birth: fetched.data.birth,
-        email: fetched.data.email
-      })
-
-      setWrite("disabled")
+      const fetched = await UpdateProfile(tokenStorage, user)
+      if (fetched) {
+        setUser({
+          name: fetched.data.name,
+          lastName: fetched.data.lastName,
+          email: fetched.data.email
+        });
+        setWrite("disabled");
+      }
     } catch (error) {
-      console.log(error)
+      setMsgError(error.message);
     }
   }
-
   return (
     <>
       <Header />
       <div className="profileDesign">
-        {!loadedData ? (
-          <div>CARGANDO</div>
-        ) : (
-          <div>
-            <CInput
-              className={`inputDesign ${
-                userError.nameError !== "" ? "inputDesignError" : ""
-              }`}
-              type={"text"}
-              placeholder={""}
-              name={"name"}
-              disabled={write}
-              value={user.name || ""}
-              onChangeFunction={(e) => inputHandler(e)}
-              onBlurFunction={(e) => checkError(e)}
-            />
-            <CInput
-              className={`inputDesign ${
-                userError.surnameError !== "" ? "inputDesignError" : ""
-              }`}
-              type={"text"}
-              placeholder={""}
-              name={"surname"}
-              disabled={write}
-              value={user.surname || ""}
-              onChangeFunction={(e) => inputHandler(e)}
-              onBlurFunction={(e) => checkError(e)}
-            />
-            <CInput
-              className={`inputDesign ${
-                userError.birthError !== "" ? "inputDesignError" : ""
-              }`}
-              type={"text"}
-              placeholder={""}
-              name={"birth"}
-              disabled={write}
-              value={user.birth || ""}
-              onChangeFunction={(e) => inputHandler(e)}
-              onBlurFunction={(e) => checkError(e)}
-            />
-            <CInput
-              className={`inputDesign ${
-                userError.emailError !== "" ? "inputDesignError" : ""
-              }`}
-              type={"email"}
-              placeholder={""}
-              name={"email"}
-              disabled={"disabled"}
-              value={user.email || ""}
-              onChangeFunction={(e) => inputHandler(e)}
-              onBlurFunction={(e) => checkError(e)}
-            />
-            <CButton
-              className={write === "" ? "cButtonGreen cButtonDesign" : "cButtonDesign"}
-              title={write === "" ? "Confirm" : "Edit"}
-              functionEmit={write === "" ? updateData : ()=> setWrite("")}
-            />
-          </div>
-        )}
+        {
+          !loadedData
+            ? (<div>LOADING</div>)
+            : (<div>
+              <CInput
+                className={`inputDesign ${userError.nameError !== "" ? "inputDesignError" : ""
+                  }`}
+                type={"text"}
+                placeholder={""}
+                name={"name"}
+                disabled={write}
+                value={user.name || ""}
+                onChangeFunction={(e) => inputHandler(e)}
+                onBlurFunction={(e) => checkError(e)}
+              />
+              <CInput
+                className={`inputDesign ${userError.emailError !== "" ? "inputDesignError" : ""
+                  }`}
+                type={"email"}
+                placeholder={""}
+                name={"email"}
+                disabled={write}
+                value={user.email || ""}
+                onChangeFunction={(e) => inputHandler(e)}
+                onBlurFunction={(e) => checkError(e)}
+              />
+              <CButton
+                className={write === "" ? "cButtonGreen cButtonDesign" : "cButtonDesign"}
+                title={write === "" ? "Confirmar" : "Edit"}
+                functionEmit={write === "" ? updatedData : () => setWrite("")}
+              />
+            </div>)
+        }
       </div>
     </>
   );
