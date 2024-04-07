3360c730fbde2bd5abd9e4b3eccce55e946f9af9
Pol <polmonteroalegre@gmail.com>
2024-04-07 16:07:31 +0200

    FEAT: Servicesjsx created and tesed

---
+++ src/pages/Services/Services.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+import { ServicesCard } from "../../components/ServicesCard/ServicesCard";
+import { GetServices } from "../../services/apiCalls";
+import "./Services.css";
+import { Header } from "../../common/Header/Header";
+
+export const Services = () => {
+    const [services, setServices] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchServices = async () => {
+            try {
+                const response = await GetServices();
+                if (response.success) {
+                    setServices(response.data);
+                } else {
+                    setError('No se pudieron recuperar los services.');
+                }
+            } catch (error) {
+                setError('Error al solicitar los services: ' + error.message);
+            }
+        };
+
+        fetchServices();
+    }, []);
+    return (
+        <>
+            <Header />
+            <div className="servicesDesign">
+                {error ? (
+                    <p>{error}</p>
+                ) : (
+                    services.map(service => (
+                        <ServicesCard
+                            key={service.id}
+                            id={service.id}
+                            serviceName={service.serviceName}
+                            description={service.description}
+                        />
+                    ))
+                )}
+            </div>
+        </>
+    );
+};
